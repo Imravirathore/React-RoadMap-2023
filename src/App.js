725f034c5fa66dbdb6d8dd4b5f31b8eb0f091f66
Imravirathore <ravirathore672@gmail.com>
2023-10-21 11:13:46 +0530

    starting react development

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>I am from App.js</h1>
+      <Footer />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+        <h5>I am from Footer</h5>
+    </>
+  )
+}
+
+export default Footer
